3231d59a0d29e1434daaffed9b43d87a2b108d39
Exoutia <jhav365@gmail.com>
2022-06-29 14:33:43 +0530

    Color Change

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from pytube import YouTube
 root = tk.Tk()
+root.attributes("-topmost", False, "-alpha", 0.9)
+root.configure(bg='blue')
 root.geometry("500x500")
 root.title("Youtube video Downloader")
 
-tk.Label(root, text= "Enter the link of the video", font=("Arial", 16)).pack()
+tk.Label(root, text= "Your Personal Youtube Video Downloader", font=("Arial", 16)).pack()
 myVar = tk.StringVar()
 myVar.set("Enter the link of the video")
-tk.Entry(root, textvariable=myVar, font=("Arial", 15)).pack()
+tk.Entry(root, textvariable=myVar, font=("Monospace", 15)).pack()
 url = tk.StringVar()
 tk.Entry(root, textvariable=url, font=("Arial", 15)).pack()
 tk.Button(root, text="Download", command=lambda: download()).pack()
